14bbfc5d3d03d1a391e88db4ac547645c37e5599
sbooyah <sbooyah@gmail.com>
2021-12-17 03:20:56 -0600

    barely an update

--- sketch.js
+++ sketch.js
@@ -2,10 +2,12 @@ let gameState = "play";
 let basa = {}
 let player1 = {}
 let player2 = {}
+let player1Score = 0;
+let player2Score = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSL, 359, 100, 100, 100);
-  basa = new Ball(windowWidth / 2, windowHeight / 2, 50, 50);
+  basa = new Ball(windowWidth / 2, windowHeight / 2, 25, 25);
   player1 = new Paddle(10, windowHeight / 2, 15, 80);
   player2 = new Paddle(windowWidth - 25, windowHeight / 2, 15, 80);
 }
@@ -13,6 +15,11 @@ function setup() {
 function draw() {
   background(40);
   if (gameState == "play") {
+    textFont('Arial');
+    textSize(80)
+    fill('white');
+    text(player1Score, windowWidth / 2 - 50, windowHeight / 5)
+    text(player2Score, windowWidth / 2 + 30, windowHeight / 5)
     basa.update();
 
     // CHECK IF BALL IS PAST PADDLES
@@ -51,6 +58,7 @@ function draw() {
     player1.draw();
     player2.draw();
     // text to show FPS
-    text(`FPS: ${Math.round(frameRate())}`, 10, 10);
+    textSize(40)
+    text(`FPS: ${Math.round(frameRate())}`, 30, 50);
   }
 }
